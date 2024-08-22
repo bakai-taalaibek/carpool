fff237608d9f3f1ce78c12f1fc3f8ac13ebc0009
bakai-taalaibek <bakai.pochta@gmail.com>
2024-08-22 09:56:15 +0600

    Make table sortable and filterable

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,26 @@
-import { Box, Table } from "@mantine/core";
+"use client";
+import { useState } from "react";
+import {
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+  rem,
+  Box,
+  Flex,
+} from "@mantine/core";
+import {
+  IconSelector,
+  IconChevronDown,
+  IconChevronUp,
+  IconSearch,
+} from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
 
-const posts = [
+const posts: RowData[] = [
   {
     role: "Водитель",
     name: "John Doe",
@@ -31,7 +51,7 @@ const posts = [
     },
     source: "London, UK",
     destination: "Manchester, UK",
-    departureDatetime: "2024-09-16T14:00:00Z",
+    departureDatetime: "2024-09-15T14:00:00Z",
     car: null,
     seats: null,
     pricePerSeat: null,
@@ -52,7 +72,7 @@ const posts = [
     },
     source: "Paris, France",
     destination: "Marseille, France",
-    departureDatetime: "2024-09-17T08:30:00Z",
+    departureDatetime: "2023-09-17T08:30:00Z",
     car: "Peugeot 208",
     seats: 4,
     pricePerSeat: 25,
@@ -73,7 +93,7 @@ const posts = [
     },
     source: "Madrid, Spain",
     destination: "Barcelona, Spain",
-    departureDatetime: "2024-09-18T09:00:00Z",
+    departureDatetime: "2025-09-18T09:00:00Z",
     car: null,
     seats: null,
     pricePerSeat: null,
@@ -171,53 +191,247 @@ const posts = [
   },
 ];
 
-const tableData = {
-  head: [
-    "Роль автора",
-    "Имя",
-    "Телефон",
-    "Откуда",
-    "Куда",
-    "Время выезда",
-    "Авто",
-    "Мест",
-    "Цена/место",
-    "Цена/салон",
-    // "Детали",
-    // "Опубликовано",
-    // "Отредактировано",
-    // "ID пользователя",
-    // "ID объявления",
-    // "Мой пост?",
-  ],
+interface RowData {
+  role: string;
+  name: string | null;
+  phone: {
+    phoneCode: number;
+    phoneNumber: number;
+  };
+  source: string;
+  destination: string;
+  departureDatetime: string;
+  car: string | null;
+  seats: number | null;
+  pricePerSeat: number | null;
+  pricePerCar: number | null;
+  details: string | null;
+  postDatetime: string;
+  lastEditedDatetime: string;
+  userId: number | null;
+  postId: number;
+  isOwnPost: boolean;
+}
 
-  body: posts.map((item) => {
-    if (item.name === null) {
-      (item.name as unknown as string) = "Анонимно";
-    }
+const headers: { name: string; value: keyof RowData }[] = [
+  {
+    name: "Кто",
+    value: "role",
+  },
+  {
+    name: "Имя",
+    value: "name",
+  },
+  {
+    name: "Телефон",
+    value: "phone",
+  },
+  {
+    name: "Откуда",
+    value: "source",
+  },
+  {
+    name: "Куда",
+    value: "destination",
+  },
+  {
+    name: "Когда",
+    value: "departureDatetime",
+  },
+  {
+    name: "Цена/место",
+    value: "pricePerSeat",
+  },
+];
 
-    item.departureDatetime =
-      new Date(item.departureDatetime).toLocaleDateString("ru") +
-      " " +
-      new Date(item.departureDatetime).toLocaleTimeString("ru", {
-        hour: "2-digit",
-        minute: "2-digit",
-      });
+interface ThProps {
+  children: React.ReactNode;
+  isReversed: boolean;
+  isCurrentSortingKey: boolean;
+  onSort(): void;
+}
 
-    return Object.values(item)
-      .splice(0, 10)
-      .map((item) =>
-        typeof item === "object" && item !== null
-          ? item.phoneCode + " " + item.phoneNumber
-          : item
-      );
-  }),
-};
+function Th({ children, isReversed, isCurrentSortingKey, onSort }: ThProps) {
+  const Icon = isCurrentSortingKey
+    ? isReversed
+      ? IconChevronUp
+      : IconChevronDown
+    : IconSelector;
+  return (
+    <Table.Th p={0}>
+      <Flex
+        w="full"
+        p="var(--mantine-spacing-xs) var(--mantine-spacing-md)"
+        style={{ cursor: "pointer" }}
+        onClick={onSort}
+        justify="space-between"
+        wrap="wrap"
+      >
+        <Text fw={500} fz="sm">
+          {children}
+        </Text>
+        <Center>
+          <Icon style={{ width: 16, height: 16 }} stroke={1.5} />
+        </Center>
+      </Flex>
+    </Table.Th>
+  );
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim();
+  return data.filter((item) =>
+    Object.values(item).some(
+      (value) =>
+        value !== null && value.toString().toLowerCase().includes(query)
+    )
+  );
+}
+
+function sortData({
+  data,
+  newSortKey,
+  isReversed,
+  searchString,
+}: {
+  data: RowData[];
+  newSortKey: keyof RowData | null;
+  isReversed: boolean;
+  searchString: string;
+}) {
+  if (!newSortKey) {
+    return filterData(data, searchString);
+  }
+  return filterData(
+    [...data].sort((a, b) => {
+      if (a[newSortKey] === null) {
+        return isReversed ? 0 : 1;
+      }
+
+      if (b[newSortKey] === null) {
+        return isReversed ? 1 : 0;
+      }
+
+      const result = a[newSortKey]
+        .toString()
+        .localeCompare(b[newSortKey].toString());
+
+      return isReversed ? -result : result;
+    }),
+    searchString
+  );
+}
 
 export default function HomePage() {
+  const [searchString, setSearchString] = useState("");
+  const [sortedData, setSortedData] = useState(posts);
+  const [sortKey, setSortKey] = useState<keyof RowData | null>(null);
+  const [isSortingReversed, setIsSortingReversed] = useState(false);
+  const router = useRouter();
+
+  const setSorting = (field: keyof RowData) => {
+    const isReversed = field === sortKey ? !isSortingReversed : false;
+    setIsSortingReversed(isReversed);
+    setSortKey(field);
+    setSortedData(
+      sortData({
+        data: posts,
+        newSortKey: field,
+        isReversed,
+        searchString,
+      })
+    );
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearchString(value);
+    setSortedData(
+      sortData({
+        data: posts,
+        newSortKey: sortKey,
+        isReversed: isSortingReversed,
+        searchString: value,
+      })
+    );
+  };
+
+  const rows = sortedData.map((row) => (
+    <Table.Tr
+      key={row.postId}
+      style={{ cursor: "pointer" }}
+      onClick={() => router.push(`/post/${row.postId}`)}
+    >
+      <Table.Td>{row.role}</Table.Td>
+      <Table.Td>{row.name}</Table.Td>
+      <Table.Td>{row.phone.phoneCode + " " + row.phone.phoneNumber}</Table.Td>
+      <Table.Td>{row.source}</Table.Td>
+      <Table.Td>{row.destination}</Table.Td>
+      <Table.Td>
+        {new Date(row.departureDatetime).toLocaleDateString("ru") +
+          ", " +
+          new Date(row.departureDatetime).toLocaleTimeString("ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+      </Table.Td>
+      <Table.Td style={{ textAlign: "center" }}>
+        {row.pricePerSeat && row.pricePerSeat + " сом"}
+      </Table.Td>
+    </Table.Tr>
+  ));
+
   return (
     <Box>
-      <Table data={tableData} />
+      <ScrollArea>
+        <TextInput
+          placeholder="Поиск по всем объявлениям"
+          mb="md"
+          leftSection={
+            <IconSearch
+              style={{ width: rem(16), height: rem(16) }}
+              stroke={1.5}
+            />
+          }
+          value={searchString}
+          onChange={handleSearchChange}
+        />
+        <Table
+          horizontalSpacing="md"
+          verticalSpacing="xs"
+          miw={700}
+          layout="fixed"
+          highlightOnHover
+        >
+          <Table.Thead>
+            <Table.Tr>
+              {headers.map((item) => (
+                <Th
+                  key={item.value}
+                  isCurrentSortingKey={sortKey === item.value}
+                  isReversed={isSortingReversed}
+                  onSort={() => setSorting(item.value)}
+                >
+                  {item.name}
+                </Th>
+              ))}
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {rows.length > 0 ? (
+              rows
+            ) : (
+              <Table.Tr>
+                <Table.Td colSpan={7}>
+                  <Text fw={500} ta="center">
+                    Nothing found
+                  </Text>
+                </Table.Td>
+              </Table.Tr>
+            )}
+          </Table.Tbody>
+        </Table>
+      </ScrollArea>
     </Box>
   );
 }
