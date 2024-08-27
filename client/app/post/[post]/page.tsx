50418a03b39d4377c4fcc456ff153488b34cd8b3
bakai-taalaibek <bakai.pochta@gmail.com>
2024-08-27 09:34:48 +0600

    Improve post page

--- client/app/post/[post]/page.tsx
+++ client/app/post/[post]/page.tsx
@@ -1,5 +1,8 @@
 import {
+  Avatar,
   Blockquote,
+  Center,
+  Divider,
   Group,
   Space,
   Stack,
@@ -12,11 +15,11 @@ import { posts } from "../../mock";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import {
-  IconEdit,
-  IconMailPlus,
+  IconAlignBoxLeftTop,
+  IconCar,
   IconMessage2,
-  IconPhone,
-  IconSquarePlus2,
+  IconPencil,
+  IconPhoneCall,
 } from "@tabler/icons-react";
 
 export default function Post({ params }: { params: { post: string } }) {
@@ -24,62 +27,100 @@ export default function Post({ params }: { params: { post: string } }) {
   dayjs.extend(relativeTime);
 
   return post ? (
-    <Stack maw={600} mx="auto" gap={33}>
-      <Stack gap={5}>
+    <Stack maw={600} mx="auto" gap={24} pt={20}>
+      <Stack gap={3}>
         <Title order={3} mx="auto">
           {dayjs(post.departureDatetime).format("D MMMM YYYY (dddd)")}
         </Title>
-        <Title order={3} c="gray.5" mx="auto">
+        <Title order={4} c="gray.5" mx="auto">
           {dayjs(post.departureDatetime).fromNow()}
         </Title>
       </Stack>
-      <Group justify="space-between" gap={0}>
-        <Text>
-          {post.role}:{" "}
+
+      <Divider />
+      <Group wrap="nowrap" justify="center">
+        <Avatar
+          // style={{
+          //   boxShadow: "0 0 3px var(--mantine-color-dark-1)",
+          // }}
+          src={post.role === "Водитель" ? "/driver8.jpg" : "/passenger2.jpg"}
+          size={90}
+          radius="lg"
+        />
+        <div>
+          <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+            {post.role}
+          </Text>
+
           {post.name ? (
-            <Text component="span">{post.name}</Text>
+            <Text fz="lg" fw={500}>
+              {post.name}
+            </Text>
           ) : (
-            <Text fs="italic" c="dark.3" component="span">
+            <Text fz="lg" fs="italic" c="dark.2">
               Имя не указано
             </Text>
           )}
-        </Text>
 
-        {/* <Group gap={5}>
-          <IconPhone size={25} color="orange" />
+          <Group wrap="nowrap" gap={10} mt={2}>
+            <IconPhoneCall stroke={1.5} size={16} />
+            <Text fz="sm" c="blue">
+              {post.phone.phoneCode} {post.phone.phoneNumber}
+            </Text>
+          </Group>
 
-          
-        </Group> */}
-        <Text>
-          Номер телефона:{" "}
-          <Text component="span" c="blue">
-            {post.phone.phoneCode} {post.phone.phoneNumber}
-          </Text>
-        </Text>
-      </Group>
-      <Timeline active={1} bulletSize={24} lineWidth={2} color="green.7">
-        <TimelineItem title={post.origin}>
-          <Text c="dimmed" size="sm">
-            Выезд ориентировочно в{" "}
-            {dayjs(post.departureDatetime).format("H:mm")}
-          </Text>
-        </TimelineItem>
-
-        <TimelineItem title={post.destination}></TimelineItem>
-      </Timeline>
-
-      {(post.car || post.seats || post.pricePerSeat || post.pricePerCar) && (
-        <Group justify="space-between">
-          {post.car && <Text>Авто: {post.car}</Text>}
-          {post.seats && <Text>Мест: {post.seats}</Text>}
-          {post.pricePerSeat && (
-            <Text>Цена/место: {post.pricePerSeat} cом</Text>
+          {post.car && (
+            <Group wrap="nowrap" gap={10} mt={2}>
+              <IconCar stroke={1.5} size={16} />
+              <Text fz="xs" c="dimmed">
+                {post.car}
+              </Text>
+            </Group>
           )}
-          {post.pricePerCar && <Text>Цена/салон: {post.pricePerCar} сом</Text>}
-        </Group>
-      )}
-      {post.details && (
+        </div>
+      </Group>
+      <Divider />
+
+      <Group justify="space-between" align="start" px="md" py="sm">
+        <Timeline active={1} bulletSize={24} lineWidth={2} color="green.7">
+          <TimelineItem title={post.origin}>
+            <Text c="dimmed" size="sm">
+              Выезд ориентировочно в{" "}
+              {dayjs(post.departureDatetime).format("H:mm")}
+            </Text>
+          </TimelineItem>
+
+          <TimelineItem title={post.destination}></TimelineItem>
+        </Timeline>
+        {(post.seats || post.pricePerSeat || post.pricePerCar) && (
+          <Stack align="end" gap={8}>
+            <Text fz={24} fw={900} lh={1} c="orange.7">
+              {post.pricePerSeat}
+              <Text ml={6} c="dark" lh={1} component="span" fz={16} fw={400}>
+                сом / место
+              </Text>
+            </Text>
+            {post.pricePerCar && (
+              <Text fz={24} fw={900} lh={1} c="blue.4">
+                {post.pricePerCar}
+                <Text ml={6} c="dark" lh={1} component="span" fz={16} fw={400}>
+                  сом / салон
+                </Text>
+              </Text>
+            )}
+
+            {post.seats && (
+              <Text c="gray.6" fz="xs" fw={500} mt={2}>
+                Свободно {post.seats} места
+              </Text>
+            )}
+          </Stack>
+        )}
+      </Group>
+
+      {post.details ? (
         <Blockquote
+          styles={{ root: { padding: 26 } }}
           color="blue.3"
           icon={<IconMessage2 />}
           cite="Комментарий автора"
@@ -87,19 +128,26 @@ export default function Post({ params }: { params: { post: string } }) {
         >
           {post.details}
         </Blockquote>
+      ) : (
+        <Divider />
       )}
       <Group justify="end" c="dark.3" gap={0}>
-        <IconMailPlus size={14} />:
-        <Text fz="xs" ml={5} mr={20} fs="italic">
-          {dayjs(post.postDatetime).fromNow()}
+        <IconAlignBoxLeftTop size={14} />
+        <Text fz="xs" fs="italic">
+          : {dayjs(post.postDatetime).fromNow()}
         </Text>
-        <IconEdit size={14} />:
-        <Text fz="xs" ml={5} fs="italic">
-          {dayjs(post.lastEditedDatetime).fromNow()}
-        </Text>
+        {post.lastEditedDatetime && (
+          <>
+            <Space w="lg" />
+            <IconPencil size={14} />
+            <Text fz="xs" fs="italic">
+              : {dayjs(post.lastEditedDatetime).fromNow()}
+            </Text>
+          </>
+        )}
       </Group>
     </Stack>
   ) : (
-    <>Данное объявление не найдено</>
+    <Center>Данное объявление не найдено</Center>
   );
 }
