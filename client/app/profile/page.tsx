af16143bc4aa15c35acc203b72187871ee11aac8
bakai-taalaibek <bakai.pochta@gmail.com>
2024-10-25 15:25:44 +0600

    Add section to profile, enhance buttons

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -16,24 +16,35 @@ import {
   Fieldset,
   Grid,
   Flex,
+  Divider,
+  Anchor,
+  ActionIcon,
+  Title,
 } from "@mantine/core";
 import { DatePickerInput, TimeInput } from "@mantine/dates";
-import { IconClock } from "@tabler/icons-react";
+import { IconClock, IconEdit, IconPencil } from "@tabler/icons-react";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function NewPage() {
   const [hasCar, setHasCar] = useState(false);
 
   return (
-    <Stack maw={600} mx="auto" gap={33} pt={30}>
-      <Avatar color="blue" radius={100} size={150} />
+    <Stack maw={600} mx="auto" gap={10} pt={30}>
+      <Avatar color="blue" radius={100} size={120} />
 
+      <Box ml="auto">
+        <ActionIcon color="gray" variant="subtle" size="lg" aria-label="Edit">
+          <IconPencil />
+        </ActionIcon>
+      </Box>
+      <Divider mb={10} />
       <div className="grid grid-cols-[auto_1fr] align-items-center gap-4">
-      <Text ta="end" h="36px" lh="36px">
+        <Text ta="end" h="36px" lh="36px">
           Имя
         </Text>
         <TextInput placeholder="Асан Болотов" />
-                <Text ta="end" h="36px" lh="36px">
+        <Text ta="end" h="36px" lh="36px">
           Номер телефона
         </Text>
         <TextInput placeholder="0 500 600 700" required />
@@ -50,10 +61,23 @@ export default function NewPage() {
         </Text>
         <Textarea required placeholder="Водитель категории В" />
       </div>
+      <Title order={4}>Изменить пароль</Title>
+      <Divider mb={10} />
+      <Button w="max-content" variant="light">
+        Сменить пароль
+      </Button>
 
-      <Button>Выйти из аккаунта</Button>
-      <Button>Сменить пароль</Button>
-      <Button>Удалить аккаунт</Button>
+      <Title order={4}>Выйти из аккаунта</Title>
+      <Divider mb={10} />
+      <Button w="max-content" variant="light">
+        Выйти из аккаунта
+      </Button>
+
+      <Title order={4}>Удалить аккаунт</Title>
+      <Divider mb={10} />
+      <Button w="max-content" variant="light" color="red">
+        Удалить аккаунт
+      </Button>
     </Stack>
   );
 }
