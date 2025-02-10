edc92f1d6094e05f6983c338fb4155522fd6b591
bakai-taalaibek <bakai.pochta@gmail.com>
2025-02-10 14:10:35 +0600

    Add search by name to filter

--- client/app/filters.tsx
+++ client/app/filters.tsx
@@ -1,27 +1,47 @@
-import { Box, Card, lighten, SegmentedControl, Text, Title } from "@mantine/core";
+import {
+  Box,
+  Card,
+  Group,
+  Input,
+  lighten,
+  SegmentedControl,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 
 function Filters() {
   return (
     <Box
+      // bg={lighten("var(--mantine-color-indigo-0)", 0.5)}
       bg="indigo.0"
+      // bg="white"
       style={{ borderRadius: "5px" }}
       mb="10px"
       p="10px"
-      bd="solid 1px var(--mantine-color-indigo-1)"
+      bd="solid 1.5px var(--mantine-color-indigo-1)"
     >
-      <Title order={3}  mt={5} mb={10}>Фильтры:</Title>
-      <Card w="fit-content">
-        <Text mb="5px">Кого показывать?</Text>
-        <SegmentedControl
-          // value={role}
-          // onChange={(role) => setRole(role)}
-          data={[
-            { label: "Всех", value: "all" },
-            { label: "Водителей", value: "drivers" },
-            { label: "Пассажиров", value: "passengers" },
-          ]}
-        />
-      </Card>
+      <Title order={3} mt={5} mb={10}>
+        Фильтры:
+      </Title>
+      <Group align="start">
+        <Card w="fit-content">
+          <Text mb="5px">Кого показывать?</Text>
+          <SegmentedControl
+            // value={role}
+            // onChange={(role) => setRole(role)}
+            data={[
+              { label: "Всех", value: "all" },
+              { label: "Водителей", value: "drivers" },
+              { label: "Пассажиров", value: "passengers" },
+            ]}
+          />
+        </Card>
+        <Card w="fit-content">
+          <Text mb="5px">Поиск по имени или фамилии:</Text>
+          <TextInput placeholder="Асан Болотов" />
+        </Card>
+      </Group>
     </Box>
   );
 }
