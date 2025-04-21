9811ede9f9d6a4485ba3564d7038e306218b5c31
bakai-taalaibek <bakai.pochta@gmail.com>
2025-04-21 21:16:28 +0600

    Add RideRole model

---
+++ dotnet/Carpool.WebApi/Models/RideRole.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Carpool.Models;
+
+[Table("RideRoles")]
+public class RideRole
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+
+    public ICollection<Post>? Posts { get; set; }
+}
