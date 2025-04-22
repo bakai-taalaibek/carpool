a3cf7095f02bc90f292082e08d92925807654648
bakai-taalaibek <bakai.pochta@gmail.com>
2025-04-22 12:39:46 +0600

    Add PostComment model

---
+++ dotnet/Carpool.WebApi/Models/PostComment.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Carpool.Models;
+
+[Table("PostComments")]
+public class PostComments
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    public int PostId { get; set; }
+    public int? UserId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Text { get; set; } = null!;
+
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+
+    public User? User { get; set; }
+    public Post Post { get; set; } = null!;
+}
