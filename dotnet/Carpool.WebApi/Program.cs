03642ea5aa2adfe94d501bb351ea418152e114c7
bakai-taalaibek <bakai.pochta@gmail.com>
2025-04-23 11:35:14 +0600

    Add postgres db

--- dotnet/Carpool.WebApi/Program.cs
+++ dotnet/Carpool.WebApi/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
