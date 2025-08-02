094df47680e67779ffe693ba821d414c7b28c0fd
bakai-taalaibek <bakai.pochta@gmail.com>
2025-08-02 10:43:08 +0600

    Create RidePostCreateDto

---
+++ dotnet/Carpool.Contracts/DTOs/RidePostCreateDto.cs
@@ -0,0 +1,32 @@
+namespace Carpool.Contracts.DTOs;
+
+public class RidePostCreateDto
+{
+    public int RideRoleId { get; set; }
+
+    public int SourceId { get; set; }
+
+    public int DestinationId { get; set; }
+
+    public DateTimeOffset DepartureDateTime { get; set; }
+
+    public int Seats { get; set; }
+
+    public int? PricePerSeat { get; set; }
+
+    public int? PricePerCar { get; set; }
+
+    public string? Comment { get; set; }
+
+    public string? AnonName { get; set; }
+
+    public string? AnonPhone { get; set; }
+
+    public string? AnonCar { get; set; }
+
+    public RideRoleFullDto? RideRole { get; set; }
+
+    public LocalityFullDto? Source { get; set; }
+
+    public LocalityFullDto? Destination { get; set; }
+}
