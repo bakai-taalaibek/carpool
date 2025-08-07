0c82779ce853412018a305287a2e94d0a10b788b
bakai-taalaibek <bakai.pochta@gmail.com>
2025-08-07 20:58:07 +0600

    Implement querying ride roles

--- client/services/rideRolesApi.ts
+++ client/services/rideRolesApi.ts
@@ -1,10 +1,31 @@
-import { RideRoleFullDto } from "../types/rideRole";
+import { RideRoleFullDto, RideRoleName } from "../types/rideRole";
 import { api } from "./api";
 
 export const rideRolesApi = api.injectEndpoints({
   endpoints: (build) => ({
-    getRideRoles: build.query<RideRoleFullDto[], void>({
+    getRideRoles: build.query<
+      {
+        rideRoles: RideRoleFullDto[];
+        rideRoleIdToNameMap: Record<number, RideRoleName>;
+        rideRoleNameToIdMap: Record<RideRoleName, number>;
+      },
+      void
+    >({
       query: () => "rideroles",
+      transformResponse: (rideRoles: RideRoleFullDto[]) => {
+        const rideRoleIdToNameMap: Record<number, RideRoleName> = {};
+        const rideRoleNameToIdMap: Record<RideRoleName, number> = {} as Record<
+          RideRoleName,
+          number
+        >;
+
+        for (const role of rideRoles) {
+          rideRoleIdToNameMap[role.id] = role.name;
+          rideRoleNameToIdMap[role.name] = role.id;
+        }
+
+        return { rideRoles, rideRoleIdToNameMap, rideRoleNameToIdMap };
+      },
     }),
   }),
 });
--- client/types/rideRole.ts
+++ client/types/rideRole.ts
@@ -2,4 +2,15 @@ export type RideRoleFullDto = {
   id: number;
   name: RideRoleName;
 };
-export type RideRoleName = "Driver" | "Passenger";
+export enum RideRoleName {
+  Driver = "Driver",
+  Passenger = "Passenger",
+}
+export enum RideRoleNameRu {
+  Driver = "Водитель",
+  Passenger = "Пассажир"
+}
+export const rideRoleNameToRuNameMap = {
+  [RideRoleName.Driver]: "Водитель",
+  [RideRoleName.Passenger]: "Пассажир",
+};
